4f82e0f914263b16e57e09771986111d3bb98669
varmac <varmac231gmail.com>
2024-02-12 13:06:24 +0000

    use fetch to get json data. improved look

--- js/app.js
+++ js/app.js
@@ -1,1 +1,22 @@
-console.log('hello world')
+
+const darkModeButton = document.querySelector(".darkModeIcon")
+const countryOneFlage = document.querySelector("#countryOneFlag");
+const countryOneName = document.querySelector("#countryOneName");
+const countryOnePopulationAmount = document.querySelector("#countryOnePopulationAmount");
+const countryOneRegion = document.querySelector("#countryOneRegion");
+const countryOneCapital = document.querySelector("#countryOneCapital");
+
+
+darkModeButton.addEventListener("click",()=>{
+    console.log('clicked')
+})
+
+fetch('./data.json')
+    .then((response)=>response.json())
+    .then((data)=>{
+        countryOneFlage.setAttribute("src", data[0].flags.svg)
+        countryOneName.textContent =  data[0].name
+        countryOnePopulationAmount.textContent = data[0].population
+        countryOneRegion.textContent = data[0].region;
+        countryOneCapital.textContent = data[0].capital
+    })
